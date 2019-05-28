228d7f5bb7f561b263bf60a318e8b25ce4d2242c
Ricardo Oliveira <ricardorodrigo.oliveira@gmail.com>
2019-05-28 18:14:55 -0300

    Nac sobre Vue 2019-04-16

---
+++ NacVue/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <br>
+    Nome:<Texto v-on:mudou="digitouNome"/>
+    Telefone:<Texto v-on:mudou="digitouFone"/>
+    <br>
+    <Button texto="Adicionar" classe="btn btn-primary" fundo="" v-on:clicou="this.inserir"/>
+    <br>
+    <div :key="indice" v-for="(valor, indice) in lista">
+      <div v-on:click="excluir(indice)">{{valor[0]}}</div>
+      <div v-on:click="excluir(indice)">{{valor[1]}}</div>
+      <Button v-on:clicou="excluir(indice)" texto="excluir"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Texto from './components/Texto.vue'
+import Button from './components/Button.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Texto,
+    Button
+  },
+  data(){
+    return { 
+      valorNome: "",
+      valorFone: "",
+      lista: []
+    }
+  },
+  methods:{
+    inserir(){ 
+      this.lista.push( [this.valorNome, this.valorFone] );
+    },
+    excluir(index){
+      this.lista.splice(index, 1);
+    },
+    digitouNome(txtNome){
+      this.valorNome = txtNome;
+    },
+    digitouFone(txtFone){
+      this.valorFone = txtFone;
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../node_modules/bootstrap/scss/bootstrap.scss';
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ NacVue/src/components/Texto.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <div class="col-md-12">
+            <input type="text" v-model="txt" v-on:keyup.enter="limpar" v-on:change="enter"/>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+       return { txt: "" }
+    },methods:{
+        enter(){
+            this.$emit("mudou", this.txt);
+        },
+        limpar(){
+            this.$emit("mudou", this.txt);
+            this.txt="";
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
